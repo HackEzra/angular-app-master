8cdbc9da27291be5b152f840c638e61370a4aac2
Dev.Ezra <92317816+HackEzra@users.noreply.github.com>
2023-02-26 05:37:13 +0300

    Add files via upload

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { APIService } from '../http/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private API: APIService) { }
+
+  getBooks() {
+    return this.API.get('books');
+  }
+
+  addBook(data) {
+    return this.API.post('books', data);
+  }
+}
